4b71b1ee1963f7258379233a8b872419a6cceef4
tsinya0315-sketch <tsinya0315@gmail.com>
2026-09-18 16:57:58 +0900

    Delete all files

--- site.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener('scroll',()=>document.querySelector('.site-header')?.classList.toggle('scrolled',scrollY>20));
-document.querySelector('.menu-toggle')?.addEventListener('click',()=>document.querySelector('.menu')?.classList.toggle('open'));
-const io=new IntersectionObserver(es=>es.forEach(e=>{if(e.isIntersecting)e.target.classList.add('show')}),{threshold:.08});document.querySelectorAll('.reveal').forEach(el=>io.observe(el));
